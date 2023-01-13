71f957504d9e12fee9a015f5873df25d9eafbdca
HassanAlhaji <halhaji84@gmail.com>
2023-01-13 16:11:12 +0000

    add logic for adding a comment

--- src/components/ReviewComments.jsx
+++ src/components/ReviewComments.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import { getCommets } from "../utils/utils";
 import CommentCard from "./CommentCard";
+import CommentForm from "./CommentForm";
 import "./ReviewComments.css";
 
 export default function ReviewComments({ reviewId }) {
   const [comments, setComments] = useState([]);
+
+  const addCommentToReview = (comment) => {
+    setComments([comment, ...comments]);
+  };
 
   useEffect(() => {
     getCommets(reviewId).then((comments) => {
@@ -15,6 +20,8 @@ export default function ReviewComments({ reviewId }) {
   return (
     <div className="review-comments">
       <h2>Comments</h2>
+
+      <CommentForm addCommentsFn={addCommentToReview} reviewId={reviewId} />
 
       {comments.map((comment) => {
         return <CommentCard comment={comment} key={comment.comment_id} />;
